68c46aadbf65f07512623f72ee02764295008d9f
GBEMAPOU Kokou <kokougbemapou@gmail.com>
2025-03-27 11:57:25 +0100

    add file main.tf

---
+++ iac/terraform/main.tf
@@ -0,0 +1,6 @@
+provider "proxmox" {
+  pm_api_url      = "https://pve.kcyberland.com/api2/json"
+  pm_api_token_id = "terraform-prov@pve!terraform-token"
+  pm_api_token_secret = "5b36dacd-1c41-4e41-ad9a-67a1ac08c547"
+  pm_tls_insecure = true # À éviter en production, utilisez un certificat valide
+}
